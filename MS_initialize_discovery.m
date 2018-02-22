c0b832e7f142642103b254fa80b834ed212f4b2a
ecarmichael <jericcarmichael@Js-MacBook-Pro-2.local>
2018-02-22 17:42:45 -0500

    Migration to new discovery identity

--- MS_initialize_discovery.m
+++ MS_initialize_discovery.m
@@ -7,9 +7,9 @@ global PARAMS
 if isunix
     PARAMS.data_dir = '/global/scratch/ecarmichael/Multisite/'; % where to find the raw data
     PARAMS.inter_dir = '/global/scratch/ecarmichael/Multisite/temp/'; % where to put intermediate files
-    PARAMS.stats_out = '/ihome/jcarmich/MS/Stats/'; % where to put the statistical output .txt
-    PARAMS.code_base_dir = '/ihome/jcarmich/Code/vandermeerlab/code-matlab/shared'; % where the codebase repo can be found
-    PARAMS.code_MS_dir = '/ihome/jcarmich/Code/EC_Multisite'; % where the multisite repo can be found
+    PARAMS.stats_out = '/dartfs-hpc/rc/home/r/f00287r/MS/Stats/'; % where to put the statistical output .txt
+    PARAMS.code_base_dir = '/dartfs-hpc/rc/home/r/f00287r/Code/vandermeerlab/code-matlab/shared'; % where the codebase repo can be found
+    PARAMS.code_MS_dir = '/dartfs-hpc/rc/home/r/f00287r/Code/EC_Multisite'; % where the multisite repo can be found
     
 else
     PARAMS.data_dir = 'G:\JK_recordings\Naris\'; % where to find the raw data
@@ -24,7 +24,7 @@ PARAMS.log = fopen([PARAMS.data_dir 'MS_log.txt'], 'w');
 PARAMS.Phases = {'pre', 'ipsi', 'contra', 'post'}; % recording phases within each session
 PARAMS.Subjects = {'R102', 'R104', 'R107', 'R108', 'R112', 'R122', 'R123'}; %list of subjects
 PARAMS.Sub_xpiri = {'R108', 'R112', 'R122', 'R123'};  % subjects with electrodes spanning the piriform cortex
-PARAMS.all_sites = {'PL', 'IL', 'OFC', 'Piri_OFC', 'NAc', 'Piri_NAc', 'CG'}; 
+PARAMS.all_sites = {'PL', 'IL', 'OFC', 'Piri_O', 'NAc', 'Piri_N', 'CG'}; 
 
 % add the required code
 addpath(genpath(PARAMS.code_base_dir));
@@ -35,3 +35,4 @@ cd(PARAMS.data_dir) % move to the data folder
 set(groot, 'DefaultAxesTickLabelInterpreter', 'none')
 set(groot, 'DefaultLegendInterpreter', 'none')
 set(groot,'defaulttextinterpreter','none');  
+Master_Multisite
