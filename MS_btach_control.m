046370a69cbc2425421dad7c437adf27aa3a3ade
ecarmichael <jericcarmichael@Js-MacBook-Pro-2.local>
2018-02-24 19:25:55 -0500

    batch control

---
+++ MS_btach_control.m
@@ -0,0 +1,8 @@
+%MS_batch_control
+
+job = batch('MS_discovery_batch', 'Pool', 8);
+wait(job)
+load(job, 'mat_all')
+
+save(['/dartfs-hpc/rc/lab/M/MeerM/EC/temp/' 'MS_mat2.mat'], 'mat_all', '-v7.3')
+
---
+++ MS_discovery_batch.m
@@ -0,0 +1,48 @@
+% for iSub = 1:length(PARAMS.Subjects)
+restoredefaultpath
+global PARAMS
+
+if isunix
+    PARAMS.data_dir = '/global/scratch/ecarmichael/Multisite/'; % where to find the raw data
+    PARAMS.inter_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/temp/'; % where to put intermediate files
+    PARAMS.stats_dir = '/dartfs-hpc/rc/home/r/f00287r/MS/Stats/'; % where to put the statistical output .txt
+    PARAMS.code_base_dir = '/dartfs-hpc/rc/home/r/f00287r/Code/vandermeerlab/code-matlab/shared'; % where the codebase repo can be found
+    PARAMS.code_MS_dir = '/dartfs-hpc/rc/home/r/f00287r/Code/EC_Multisite'; % where the multisite repo can be found
+    
+else
+    PARAMS.data_dir = 'G:\JK_recordings\Naris\'; % where to find the raw data
+    PARAMS.inter_dir = 'G:\JK_recordings\Naris\Multisite\temp\'; % where to put intermediate files
+    PARAMS.stats_dir = 'G:\JK_recordings\Naris\Multisite\temp\Stats\'; % where to put the statistical output .txt
+    PARAMS.code_base_dir = 'D:\Users\mvdmlab\My_Documents\GitHub\vandermeerlab\code-matlab\shared'; % where the codebase repo can be found
+    PARAMS.code_MS_dir = 'D:\Users\mvdmlab\My_Documents\GitHub\EC_Multisite'; % where the multisite repo can be found
+end
+% log the progress
+PARAMS.log = fopen([PARAMS.inter_dir 'MS_log.txt'], 'w');
+% define subjects, phases, 
+PARAMS.Phases = {'pre', 'ipsi', 'contra', 'post'}; % recording phases within each session
+PARAMS.Subjects = {'R102', 'R104', 'R107', 'R108', 'R112', 'R122', 'R123'}; %list of subjects
+PARAMS.Sub_xpiri = {'R108', 'R112', 'R122', 'R123'};  % subjects with electrodes spanning the piriform cortex
+PARAMS.all_sites = {'PL', 'IL', 'OFC', 'Piri_O', 'NAc', 'Piri_N', 'CG'}; 
+
+% add the required code
+addpath(genpath(PARAMS.code_base_dir));
+addpath(genpath(PARAMS.code_MS_dir));
+cd(PARAMS.data_dir) % move to the data folder
+
+% formatting
+set(groot, 'DefaultAxesTickLabelInterpreter', 'none')
+set(groot, 'DefaultLegendInterpreter', 'none')
+set(groot,'defaulttextinterpreter','none');  
+
+iSub = 2;
+sess_list = fieldnames(Events.(PARAMS.Subjects{iSub}));
+for iSess = 1:length(sess_list)
+    %             fprintf(PARAMS.log,['\nExtracting phase slope ' PARAMS.Subjects{iSub} '  ' sess_list{iSess}]);
+    mat_all{iSub,iSess} = MS_get_phase_metrics([], Events.(PARAMS.Subjects{iSub}).(strrep(sess_list{iSess}, '-', '_')), data.(PARAMS.Subjects{iSub}).(strrep(sess_list{iSess}, '-', '_')));
+    
+    %             fprintf(PARAMS.log, '...complete');
+end
+%     end
+
+
+% save([PARAMS.inter_dir 'MS_mat2.mat'], 'mat_all', '-v7.3')
