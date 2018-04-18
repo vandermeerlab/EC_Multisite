dd0db7afb02706417c155484d936e9a9671cb1ee
ecarmichael <jericcarmichael@Js-MacBook-Pro-2.local>
2018-04-18 15:48:36 -0400

    added data split function for DC

---
+++ MS_init_split_data.m
@@ -0,0 +1,59 @@
+%% MS_initialize: split the all_data, events, and naris structs into invidivual subjects
+tic
+clear all; close all
+restoredefaultpath
+global PARAMS
+
+if isunix
+    PARAMS.data_dir = '/global/scratch/ecarmichael/Multisite/'; % where to find the raw data
+    PARAMS.inter_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/temp/'; % where to put intermediate files
+    PARAMS.stats_dir = '/dartfs-hpc/rc/home/r/f00287r/MS/Stats/'; % where to put the statistical output .txt
+    PARAMS.code_base_dir = '/dartfs-hpc/rc/home/r/f00287r/Code/vandermeerlab/code-matlab/shared'; % where the codebase repo can be found
+    PARAMS.code_MS_dir = '/dartfs-hpc/rc/home/r/f00287r/Code/EC_Multisite'; % where the multisite repo can be found
+    
+else
+    PARAMS.data_dir = 'G:\JK_recordings\Naris\'; % where to find the raw data
+    PARAMS.inter_dir = 'G:\JK_recordings\Naris\Multisite\temp\'; % where to put intermediate files
+    PARAMS.stats_dir = 'G:\JK_recordings\Naris\Multisite\temp\Stats\'; % where to put the statistical output .txt
+    PARAMS.code_base_dir = 'D:\Users\mvdmlab\My_Documents\GitHub\vandermeerlab\code-matlab\shared'; % where the codebase repo can be found
+    PARAMS.code_MS_dir = 'D:\Users\mvdmlab\My_Documents\GitHub\EC_Multisite'; % where the multisite repo can be found
+end
+% log the progress
+PARAMS.log = fopen([PARAMS.inter_dir 'MS_log.txt'], 'w');
+% define subjects, phases, 
+PARAMS.Phases = {'pre', 'ipsi', 'contra', 'post'}; % recording phases within each session
+PARAMS.Subjects = {'R102', 'R104', 'R107', 'R108', 'R112', 'R122', 'R123'}; %list of subjects
+PARAMS.Sub_xpiri = {'R108', 'R112', 'R122', 'R123'};  % subjects with electrodes spanning the piriform cortex
+PARAMS.all_sites = {'PL', 'IL', 'OFC', 'PiriO', 'NAc', 'PiriN', 'CG'}; 
+
+% add the required code
+addpath(genpath(PARAMS.code_base_dir));
+addpath(genpath(PARAMS.code_MS_dir));
+cd(PARAMS.data_dir) % move to the data folder
+
+% formatting
+set(groot, 'DefaultAxesTickLabelInterpreter', 'none')
+set(groot, 'DefaultLegendInterpreter', 'none')
+set(groot,'defaulttextinterpreter','none');  
+
+%% load and split the subjects
+load([PARAMS.inter_dir 'MS_data.mat'])
+load([PARAMS.inter_dir 'MS_naris.mat'])
+load([PARAMS.inter_dir 'MS_events.mat'])
+d_t = data; 
+e_t = Events;
+n_t = Naris;
+
+for iSub = 1:length(PARAMS.Subjects)
+    data = d_t.(PARAMS.Subjects{iSub});
+    data = e_t.(PARAMS.Subjects{iSub});
+    Naris = n_t.(PARAMS.Subjects{iSub});
+
+    save([PARAMS.inter_dir PARAMS.Subjects{iSub} '_Data'], data, '-v7.3')
+    save([PARAMS.inter_dir PARAMS.Subjects{iSub} '_Events'], Events, '-v7.3')
+    save([PARAMS.inter_dir PARAMS.Subjects{iSub} '_Naris'], Naris, '-v7.3')
+clear data Events Naris
+end
+    
+    
+    
