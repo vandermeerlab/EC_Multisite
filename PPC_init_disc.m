9beb6ad656bc8c71ed229e983138ba9b821f66bb
ecarmichael <jericcarmichael@Js-MacBook-Pro-2.local>
2018-05-28 15:32:24 -0400

    modifed PPC_init_disc to deal with new figure style and saving path

--- PPC_init_disc.m
+++ PPC_init_disc.m
@@ -11,13 +11,18 @@ addpath('/dartfs-hpc/rc/home/r/f00287r/Code/fieldtrip')
 % ft_defaults
 % addpath(genpath('/Users/jericcarmichael/Documents/GitHub/EC_Multisite'))
 ft_defaults
+data_root = '/dartfs-hpc/rc/lab/M/MeerM/EC';
+mkdir(data_root, 'PPC_figs')
+nShuffle = 100; 
+%%
+
 disp('112 TT7 ...')
 cfg_in = [];
 cfg_in.dataset = {'CSC16.ncs'};
-cfg_in.data_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R112-2017-08-01_post';
-cfg_in.inter_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R112-2017-08-01_post';
+cfg_in.data_dir = [data_root '/R112-2017-08-01_post'];
+cfg_in.inter_dir = [data_root '/PPC_figs'];
 cfg_in.phase = 1; % corresponds to the first recording phase ('pre').  2 = 'task', 3 = 'post'.
-cfg_in.shuffle = 500; 
+cfg_in.shuffle = nShuffle; 
 cfg_in.plot = 1;
 cfg_in.min_nSpikes = 500; 
 cfg_in.spike_id = 'TT7_cut'; 
@@ -27,10 +32,10 @@ disp('complete')
 disp('112 St1 ...')
 cfg_in = [];
 cfg_in.dataset = {'CSC1.ncs'};
-cfg_in.data_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R112-2017-08-01_post';
-cfg_in.inter_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R112-2017-08-01_post';
+cfg_in.data_dir = [data_root '/R112-2017-08-01_post'];
+cfg_in.inter_dir = [data_root '/PPC_figs'];
 cfg_in.phase = 1; % corresponds to the first recording phase ('pre').  2 = 'task', 3 = 'post'.
-cfg_in.shuffle = 500; 
+cfg_in.shuffle = nShuffle; 
 cfg_in.plot = 1;
 cfg_in.min_nSpikes = 500; 
 cfg_in.spike_id = 'ST1_cut'; 
@@ -41,10 +46,10 @@ disp('complete')
 disp('112 ST6 ...')
 cfg_in = [];
 cfg_in.dataset = {'CSC12.ncs'};
-cfg_in.data_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R112-2017-08-01_post';
-cfg_in.inter_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R112-2017-08-01_post';
+cfg_in.data_dir = [data_root '/R112-2017-08-01_post'];
+cfg_in.inter_dir = [data_root '/PPC_figs'];
 cfg_in.phase = 1; % corresponds to the first recording phase ('pre').  2 = 'task', 3 = 'post'.
-cfg_in.shuffle = 500; 
+cfg_in.shuffle = nShuffle; 
 cfg_in.plot = 1;
 cfg_in.min_nSpikes = 500; 
 cfg_in.spike_id = 'ST6_cut'; 
@@ -56,10 +61,10 @@ disp('complete')
 disp('108 ST3 ...')
 cfg_in = [];
 cfg_in.dataset = {'CSC5.ncs'};
-cfg_in.data_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R108-2017-08-04_pre';
-cfg_in.inter_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R108-2017-08-04_pre';
+cfg_in.data_dir = [data_root '/R108-2017-08-04_pre'];
+cfg_in.inter_dir = [data_root '/PPC_figs'];
 cfg_in.phase = 1; % corresponds to the first recording phase ('pre').  2 = 'task', 3 = 'post'.
-cfg_in.shuffle = 500; 
+cfg_in.shuffle = nShuffle; 
 cfg_in.plot = 1;
 cfg_in.min_nSpikes = 500; 
 cfg_in.spike_id = 'ST3_cut'; 
@@ -70,10 +75,10 @@ disp('complete')
 disp('108 TT7 ...')
 cfg_in = [];
 cfg_in.dataset = {'CSC16.ncs'};
-cfg_in.data_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R108-2017-08-04_pre';
-cfg_in.inter_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R108-2017-08-04_pre';
+cfg_in.data_dir = [data_root '/R108-2017-08-04_pre'];
+cfg_in.inter_dir = [data_root '/PPC_figs'];
 cfg_in.phase = 1; % corresponds to the first recording phase ('pre').  2 = 'task', 3 = 'post'.
-cfg_in.shuffle = 500; 
+cfg_in.shuffle = nShuffle; 
 cfg_in.plot = 1;
 cfg_in.min_nSpikes = 500; 
 cfg_in.spike_id = 'TT7_cut'; 
@@ -84,10 +89,10 @@ disp('complete')
 disp('123 TT1 ...')
 cfg_in = [];
 cfg_in.dataset = {'CSC1.ncs'};
-cfg_in.data_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R123-2017-02-27_pre2';
-cfg_in.inter_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R123-2017-02-27_pre2';
+cfg_in.data_dir = [data_root '/R123-2017-02-27_pre2'];
+cfg_in.inter_dir = [data_root '/PPC_figs'];
 cfg_in.phase = 1; % corresponds to the first recording phase ('pre').  2 = 'task', 3 = 'post'.
-cfg_in.shuffle = 500; 
+cfg_in.shuffle = nShuffle; 
 cfg_in.plot = 1;
 cfg_in.min_nSpikes = 500; 
 cfg_in.spike_id = 'TT1_cut'; 
@@ -98,10 +103,10 @@ disp('complete')
 disp('123 TT4 ...')
 cfg_in = [];
 cfg_in.dataset = {'CSC16.ncs'};
-cfg_in.data_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R123-2017-02-27_pre2';
-cfg_in.inter_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R123-2017-02-27_pre2';
+cfg_in.data_dir = [data_root '/R123-2017-02-27_pre2'];
+cfg_in.inter_dir = [data_root '/PPC_figs'];
 cfg_in.phase = 1; % corresponds to the first recording phase ('pre').  2 = 'task', 3 = 'post'.
-cfg_in.shuffle = 500; 
+cfg_in.shuffle = nShuffle; 
 cfg_in.plot = 1;
 cfg_in.min_nSpikes = 500; 
 cfg_in.spike_id = 'TT4_cut'; 
